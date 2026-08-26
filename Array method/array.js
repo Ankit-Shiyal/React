3febf4c314bd484aa75f3554dd29a71d40e4f005
ankit shiyal <ankitshiyal2005@gmail.com>
2026-08-26 10:41:58 +0530

    array method added

---
+++ Array method/array.js
@@ -0,0 +1,14 @@
+
+//1 
+let fruits =["apple", "orange", "banana", "strawberry", "guava"]
+
+console.log(typeof fruits)
+
+console.log("fruits name :", fruits)
+
+//2
+
+let number = ["11", "12" ,"13", "14", "15"]
+console.log("number :", number)
+
+
