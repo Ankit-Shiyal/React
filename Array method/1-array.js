56552cfc677541644f18c6da08819de5622c04d6
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-08-27 10:03:54 +0530

    array method

--- Array method/1-array.js
+++ Array method/1-array.js
@@ -12,3 +12,17 @@ let number = ["21", "22" ,"23", "24", "25"]
 console.log("number :", number)
 
 
+// // 
+// //1 
+// let cars = ["BMW", "Audi", "Mercedes", "Tesla", "Toyota"];
+
+// console.log(typeof cars);
+
+// console.log("Car names :", cars);
+
+// //2
+
+// let number = ["21", "22" ,"23", "24", "25"]
+// console.log("number :", number)
+
+
