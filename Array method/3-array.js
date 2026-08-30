04c4a6709d9cdb0b6048aa0040019245411d4e4d
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-08-30 14:33:24 +0530

    Clean up splice() example comments in 3-array.js
    Removed commented-out code related to the splice() method.

--- Array method/3-array.js
+++ Array method/3-array.js
@@ -46,20 +46,3 @@ console.log("after splice",fruits.length)
 
 
 
-// // splice() is used to add, remove, and replace elements in an array
-
-// let fruits =["apple", "Pineapple", "banana", "strawberry", "Mango","kiwi", "papaya" ]
-
-// console.log("before splice",fruits.length)
-
-// fruits.splice(2, 0,"grapes");
-
-// console.log("added ", fruits)
-
-// fruits.splice(4,1,"guvava")
-// console.log("replace ", fruits)
-
-// fruits.splice(4,2)
-// console.log("remove ", fruits)
-
-// console.log("after splice",fruits.length)
