3545554a3b7bd1a043cd1587cf70f216293874f3
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-08-31 12:56:31 +0530

    some change added

--- Array method/1-array.js
+++ Array method/1-array.js
@@ -11,18 +11,3 @@ console.log("Car names :", cars);
 let number = ["21", "22" ,"23", "24", "25"]
 console.log("number :", number)
 
-
-// // 
-// //1 
-// let cars = ["BMW", "Audi", "Mercedes", "Tesla", "Toyota"];
-
-// console.log(typeof cars);
-
-// console.log("Car names :", cars);
-
-// //2
-
-// let number = ["21", "22" ,"23", "24", "25"]
-// console.log("number :", number)
-
-
--- Array method/2-array.js
+++ Array method/2-array.js
@@ -26,30 +26,3 @@ console.log("favorite car :", cars[1]);
 console.log("Modify array :", cars);
 
 
-
-// // Accessing Array Elements using Index
-
-// let cities = ["Bhavnagar", "Ahmedabad", "Surat", "Rajkot", "Vadodara"];
-
-// console.log("array :", cities);
-// console.log("first city :", cities[0]);
-// console.log("favorite city :", cities[3]);
-// console.log("last city :", cities[cities.length - 1]);
-
-// // Modifying Array Elements
-
-// cities[3] = "Mumbai";
-// console.log("favorite city :", cities[3]);
-// console.log("Modify array :", cities);
-
-// // 
-// let cars = ["BMW", "Audi", "Tesla", "Toyota", "Mercedes"];
-
-// console.log("array :", cars);
-// console.log("first car :", cars[0]);
-// console.log("favorite car :", cars[1]);
-// console.log("last car :", cars[cars.length - 1]);
-
-// cars[1] = "Honda";
-// console.log("favorite car :", cars[1]);
-// console.log("Modify array :", cars);
