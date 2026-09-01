6e76eafae041d8b8f05683a838735cdbdf86253e
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-09-01 10:56:19 +0530

    fragment and JSX added

---
+++ 01_Intro/new project/src/components/01_JSX.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const JSX = () => {
+  // without jsx
+  let element = React.createElement("h1", null, "without jsx");
+
+  // with jsx
+  element = <h1>this is with jsx</h1>;
+
+  return <>{element}</>;
+};
+
+export default JSX;
---
+++ 01_Intro/new project/src/components/02_fragments.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Fragments = () => {
+  return (
+    // <div>
+    //   <h1>fragments using div</h1>
+    // </div>
+
+    // <React.Fragment>
+    //     <h1>fragments using React.Fragment</h1>
+    // </React.Fragment>
+
+    <>
+      <h1>fragments</h1>
+    </>
+  );
+};
+
+export default Fragments;
