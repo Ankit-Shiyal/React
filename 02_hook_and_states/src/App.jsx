fa0e865bce37b5f8c77c4a38c87ea800a73a87d8
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-09-02 11:01:48 +0530

    hook and states added

---
+++ 02_hook_and_states/src/App.jsx
@@ -0,0 +1,35 @@
+
+import React from "react";
+import States from "./components/01_states";
+
+
+const App = () => {
+  return (
+    <>
+      <h1>hello</h1>
+
+    <States/>
+      </>
+  );
+};
+
+export default App
+
+
+
+
+// import React from "react";
+// import States from "./components/01_states";
+
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>hello</h1>
+
+//     <States/>
+//       </>
+//   );
+// };
+
+// export default App
---
+++ 02_hook_and_states/src/components/01_states.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const States = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+
+    // setCount((c) => c + 1)
+  };
+
+  return (
+    <>
+      <h1>count value {count}</h1>
+
+      <button onClick={handleIncrement}> increment +1</button>
+
+    </>
+  );
+};
+
+export default States;
+
+
+
+
+// import { useState } from "react";
+
+// const States = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handleIncrement = () => {
+//     setCount(count + 1);
+
+//     // setCount((c) => c + 1)
+//   };
+
+//   return (
+//     <>
+//       <h1>count value {count}</h1>
+
+//       <button onClick={handleIncrement}> increment +1</button>
+
+//     </>
+//   );
+// };
+
+// export default States;
