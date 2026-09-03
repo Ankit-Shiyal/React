59f02e4317a017ecc20ec2e0f660a800470ed117
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-09-03 23:58:07 +0530

    Fix export statement for States component

--- 02_hook_and_states/src/components/01_states.jsx
+++ 02_hook_and_states/src/components/01_states.jsx
@@ -18,29 +18,4 @@ const States = () => {
   );
 };
 
-export default States;
-
-
-
-
-// import { useState } from "react";
-
-// const States = () => {
-//   const [count, setCount] = useState(0);
-
-//   const handleIncrement = () => {
-//     setCount(count + 1);
-
-//   };
-
-//   return (
-//     <>
-//       <h1>count value {count}</h1>
-
-//       <button onClick={handleIncrement}> increment +1</button>
-
-//     </>
-//   );
-// };
-
-// export default States;
+export default Stattes
