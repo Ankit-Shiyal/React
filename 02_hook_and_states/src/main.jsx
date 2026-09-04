c3bbb88d68a12e0c39642c6993409bb3962352cb
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-09-04 21:25:07 +0530

    Clean up main.jsx by removing comments and fixing import
    Removed commented-out code and fixed import statement.

--- 02_hook_and_states/src/main.jsx
+++ 02_hook_and_states/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+roimport { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,18 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
-
-
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
