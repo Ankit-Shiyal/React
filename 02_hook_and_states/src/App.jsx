c860dfaa33ab62f3c9152ec6c881709423b80eba
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-09-05 20:09:44 +0530

    Clean up App.jsx by removing unused code
    Removed commented-out code for clarity.

--- 02_hook_and_states/src/App.jsx
+++ 02_hook_and_states/src/App.jsx
@@ -15,21 +15,3 @@ const App = () => {
 
 export default App
 
-
-
-
-// import React from "react";
-// import States from "./components/01_states";
-
-
-// const App = () => {
-//   return (
-//     <>
-//       <h1>hello</h1>
-
-//     <States/>
-//       </>
-//   );
-// };
-
-// export default App
