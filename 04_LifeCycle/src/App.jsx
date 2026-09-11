aff6f62cf8010d4cc7640109e41911876bf3646d
ankit shiyal <ankitshiyal2005@gmail.com>
2026-09-11 11:37:16 +0530

    useEffect table added

--- 04_LifeCycle/src/App.jsx
+++ 04_LifeCycle/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import LifeCycle from './components/01_lifeCycle'
+import Users from './components/02_fetchAPI'
 
 const App = () => {
   return (
     <>
       <h1>Hello</h1>
-      <LifeCycle />
+      {/* <LifeCycle /> */}
+
+      <Users/>
     </>
   )
 }
---
+++ 04_LifeCycle/src/components/02_fetchAPI.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      const data = await res.json();
+
+    //   console.log(data);
+
+      setUsers(data);
+    }
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>address</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((u) => {
+            return (
+              <tr key={u.id}>
+                <td>{u.id}</td>
+                <td>{u.name}</td>
+                <td>{u.email}</td>
+                <td>{u.address["city"]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Users;
