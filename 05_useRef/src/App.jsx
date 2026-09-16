00b54a6ebecd5a3d8f8bdc17fbcceaa699be4b1e
ankit shiyal <ankitshiyal2005@gmail.com>
2026-09-16 11:17:39 +0530

    useRef code added

---
+++ 05_useRef/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseRef from './components/01_useRef'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <UseRef/>
+    </>
+  )
+}
+
+export default App
---
+++ 05_useRef/src/components/01_useRef.jsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+
+const UseRef = () => {
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Enter your name"
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleFocus}>Focus</button>
+    </>
+  );
+};
+
+export default UseRef;
