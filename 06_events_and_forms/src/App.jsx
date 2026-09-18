424805e8476786874b8907687d46ae36c999cbb0
ankit shiyal <ankitshiyal2005@gmail.com>
2026-09-18 10:36:57 +0530

    OnClick and Events added

---
+++ 06_events_and_forms/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import OnClick from './components/01_OnClick'
+import Events from './components/02_Events'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello</h1>
+    {/* <OnClick/> */}
+    <Events/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ 06_events_and_forms/src/components/01_OnClick.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OnClick = () => {
+  return <button onClick={() => alert("you click")}> click</button>;
+};
+
+export default OnClick;
